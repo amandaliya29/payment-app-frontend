1124792d39ef74b3e353349149fb9432efd02ce4
Ashish Mandaliya <ajmandaliya290@gmail.com>
2025-08-24 16:21:28 +0530

    Resolved conflict issue

--- src/component/Input.js
+++ src/component/Input.js
@@ -68,9 +68,6 @@ const Input = ({
 
       {/* Normal Input */}
       {!secureTextEntry && !isSearch && (
-    <View>
-      {/* Normal Input */}
-      {!secureTextEntry && (
         <>
           {isPhoneInput ? (
             <View style={styles.phoneContainer}>
@@ -121,7 +118,6 @@ const Input = ({
 
       {/* Secure Input (Password field) */}
       {secureTextEntry && !isSearch && (
-      {secureTextEntry && (
         <View style={styles.inputContainer}>
           <TextInput
             value={value}
