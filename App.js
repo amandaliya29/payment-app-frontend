230c2148fa2e347d789448d86414f5cc9e8bd57c
Ashish Mandaliya <ajmandaliya290@gmail.com>
2025-08-25 23:33:59 +0530

    Add multi lang support

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './src/utils/language/i18n';
 import StackNavigation from './src/navigation/StackNavigation';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <StackNavigation />
-    </NavigationContainer>
+    <I18nextProvider i18n={i18n}>
+      <NavigationContainer>
+        <StackNavigation />
+      </NavigationContainer>
+    </I18nextProvider>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
